05d0618b4217dec1f3e514df42bc686bec842dbd
Gemma Yubos <142143749+gera17-jpg@users.noreply.github.com>
2024-07-17 21:49:52 +0800

    Update rocket.js

--- rocket.js
+++ rocket.js
@@ -1,34 +1,76 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function() {
 
-    let engineBtn=  document.querySelector(".engineBtn");
-    let rocketStatus ={engine: engineBtn.value, speed: 0};
+    let engineBtn = document.querySelector(".engineBtn");
+    let rocketStatus = { engine: engineBtn.value, speed: 0 };
     let launcherBorder = document.getElementsByClassName("launch")[0];
     let destination = document.getElementsByClassName("destinationC")[0];
     let launchElement = document.querySelector(".launch");
-    let launchStatus = "clicked";
-    
-    function turnOnEngine(){
-        if(rocketStatus.engine === "Off"){
+    let speed = document.querySelector(".speedInput");
+   // let launchStatus = "unclickable";
+
+    function turnOnEngine() {
+        if (rocketStatus.engine === "Off") {
             console.log("Turning the engine on");
-            document.querySelector(".engineBtn").value = "On";
-            rocketStatus.engine="On";
-            launcherBorder.style.border = "10px solid green";
-            destination.style.display="inline";
-        }else{
+            engineBtn.value = "On";
+            speed.value = "9000";
+            rocketStatus.engine = "On";
+            speed.style.color = "green";
+            launcherBorder.style.border = "10px solid yellow";
+            launchStatus = "clickable";
+            launchElement.style.backgroundColor = "teal";
+            destination.style.display = "inline";
+        } else {
             console.log("Turning the engine off");
-            document.querySelector(".engineBtn").value = "Off";
-            rocketStatus.engine="Off";
+            engineBtn.value = "Off";
+            speed.value = "0000";
+            rocketStatus.engine = "Off";
+            speed.style.color = "";
             launcherBorder.style.border = "";
-            destination.style.display= "";
+            launchElement.style.backgroundColor = "grey";
+            destination.style.display = "none";
+          //  launchStatus = "unclickable";
         }
     }
-    
-    function launchRocket() {
-        if (launchStatus === "clicked") {
-            launchElement.style.border = "2px solid yellow"; // Set the border
+
+   /* function launchRocket() {
+        if (rocketStatus.engine === "On" && launchStatus === "clickable") {
+            console.log("Launching the rocket");
+            launchElement.style.border = "10px solid green"; // Set the border
             launchElement.style.backgroundColor = "green"; // Set the background color
+        } else {
+            console.log("Rocket launch not possible. Ensure engine is on and ready to launch.");
+        }
+    }*/
+
+    function setDistance(){
+        let chosenDes = document.querySelector('input[name="des"] :checked').value.trim();
+        let distanceInput = document.querySelector(".distanceInput");
+        switch (chosenDes.toLowerCase()){
+            case "mars":
+                distanceInput.value = "54600000";
+                break;
+            case "moon":
+                distanceInput.value = "384400";
+                break;
+            case "saturn ":
+                distanceInput.value = "1352600000 ";
+                break;
+            case "uranus":
+                distanceInput.value = "3003900000 ";
+                break;
+            case "jupiter ":
+                distanceInput.value = "714000000000 ";
+                break;
+            default:
+            distanceInput.value = "";
+            console.log("No destination selected");
         }
     }
-    document.querySelector(".engineBtn").addEventListener("click", turnOnEngine);
-    launchElement.addEventListener("click", launchRocket);
-})
+
+    document.querySelectorAll('input[name="des"]').forEach(radio => {
+    radio.addEventListener("change", setDistance);
+    });
+    engineBtn.addEventListener("click", turnOnEngine);
+    launchElement.addEventListener("click", launchRocket);    
+
+});
