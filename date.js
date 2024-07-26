e65a74dbb05e41c416e7d60a1f380e83412bc9bd
Gemma Yubos <142143749+gera17-jpg@users.noreply.github.com>
2024-07-26 18:30:40 +0800

    Create date.js

---
+++ date.js
@@ -0,0 +1,6 @@
+let date = new Date();
+console.log(date);    //Fri Jul 26 2024 18:25:33 GMT+0800 (Philippine Standard Time)
+
+let date = new Date();
+let year = date.getFullYear();
+console.log("We're currently living in the year:",year);     //We're currently living in the year: 2024
