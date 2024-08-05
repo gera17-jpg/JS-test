f36f3373ac636039671641901effe908536dfa9b
Gemma Yubos <142143749+gera17-jpg@users.noreply.github.com>
2024-08-05 21:17:54 +0800

    Update rocket.js

--- rocket.js
+++ rocket.js
@@ -5,6 +5,10 @@ let destination;
 let launchElement;
 let speed;
 let distanceInput;
+let plus100;
+let plus500;
+let minus100;
+let minus500;
 
 function turnOnEngine() {
     if (rocketStatus.engine === "Off") {
@@ -65,6 +69,36 @@ function rocketLaunch() {
   
     }
 }
+    
+function add100 () {
+    let btn ="clicked";  
+    if (btn === "clicked"){
+        speed.value = parseInt(speed.value) + 100;
+        distanceInput.value = parseInt(distanceInput.value) - 100;
+    } 
+}
+
+function add500 () {
+    let btn ="clicked";  
+    if (btn === "clicked"){
+        speed.value = parseInt(speed.value) + 500;
+        distanceInput.value = parseInt(distanceInput.value) - 500;
+    } 
+}
+
+function dec100 () {
+    let btn ="clicked";  
+    if (btn === "clicked"){
+        speed.value = parseInt(speed.value) - 100;
+    } 
+}
+
+function dec500 () {
+    let btn ="clicked";  
+    if (btn === "clicked"){
+        speed.value = parseInt(speed.value) - 500;
+    } 
+}
 
 document.addEventListener("DOMContentLoaded", function() {
     engineBtn = document.querySelector(".engineBtn");
@@ -74,12 +108,19 @@ document.addEventListener("DOMContentLoaded", function() {
     launchElement = document.querySelector(".launch");
     speed = document.querySelector(".speedInput");
     distanceInput = document.querySelector(".distanceInput");
+    plus100 = document.querySelector(".p100");
+    plus500 = document.querySelector(".p500");
+    minus100 = document.querySelector(".m100");
+    minus100 = document.querySelector(".m500");
+
 
     engineBtn.addEventListener("click", turnOnEngine);
     launchElement.addEventListener("click", rocketLaunch);
     document.querySelectorAll('input[name="des"]').forEach(radio => {
         radio.addEventListener("change", setDistance);
     });
+    plus100.addEventListener("click",add100);
+    plus500.addEventListener("click",add500);
+    minus100.addEventListener("click",dec100);
+    minus500.addEventListener("click",dec500);
 });
-
-/*create a function for the  -100 ,-500 ,+100 & + 500
